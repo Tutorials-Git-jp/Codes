527992ff1d0a85a1cc0ec839805f06809eb80e6b
Najifa tabassum <119313705+najifatabassum01@users.noreply.github.com>
2022-12-29 21:09:14 +0600

    Add files via upload

--- FXMLDocumentController.java
+++ FXMLDocumentController.java
@@ -2,14 +2,18 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXML2.java to edit this template
  */
-package javafxapplication3;
+package projectmanagementsystem;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
 
 /**
  *
@@ -17,18 +21,24 @@ import javafx.scene.control.Label;
  */
 public class FXMLDocumentController implements Initializable {
     
-    @FXML
-    private Label label;
     
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+    private Button loginbtn;
+
+    @FXML
+    private PasswordField passward;
+    @FXML
+    private AnchorPane main_form;
+
+    @FXML
+    private TextField username;
+    public void close(){
+        System.exit(0);
     }
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+      
     }    
     
 }
